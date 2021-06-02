e1c726ab6802666638986ca2651d8e0ac7d04fd7
cammneto <cammneto@gmail.com>
2021-06-01 22:59:16 -0300

    new file:   csv/deepvalue_filtered.csv 	new file:   csv/deepvalue_fundamentus.csv 	new file:   csv/deepvalue_investsite.csv 	new file:   csv/deepvalue_statusinv.csv 	new file:   csv/fundamentus.csv 	new file:   csv/investsite.csv 	new file:   csv/statusinvest.csv 	new file:   deepvalue_filter.py 	new file:   ss_fundamentus.py 	new file:   ss_investsite.py 	new file:   ss_statusinvest.py 	new file:   tickers.dat

---
+++ ss_fundamentus.py
@@ -0,0 +1,87 @@
+from lxml import html
+import pandas as pd
+import requests
+import datetime
+import shutil
+
+# Read file with stocks tickers
+def tickers(InputFile):
+    infile = open(InputFile, "r")
+    tickers = [line.split() for line in infile]
+    ticker = []
+    for i in tickers:
+        ticker.append(i[0])
+    return ticker
+
+def page_tables(ticker):
+    header = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36","X-Requested-With": "XMLHttpRequest"}
+    url  = 'https://fundamentus.com.br/detalhes.php?papel='+ticker
+    page = requests.get(url, headers=header)
+    # Get all the tables from the html content
+    for p in page:
+     try:
+         tables = pd.read_html(page.content)
+         return tables
+     except ValueError:
+         pass
+
+
+def situacao(ticker):
+    url = 'https://www.investsite.com.br/principais_indicadores.php?cod_negociacao='+ticker
+    page = requests.get(url)
+    # Get all the tables from the html content
+    for p in page:
+     try:
+         tables = pd.read_html(page.content)
+         db = [tables[1][0],tables[1][1]]
+         print('Situação --->',db[1][3])
+         situacao = db[1][3]
+         return situacao
+     except ValueError:
+         pass
+
+stocks = tickers('tickers.dat')
+names,tickers,price,evebit,m_ebit,volume,sit=[],[],[],[],[],[],[]
+for i in range(len(stocks)):
+    tables = page_tables(stocks[i])
+    print('Scraping',i+1, 'out of', len(stocks),'from: fundamentus.com.br')
+    if tables==None:
+        print('Ativo:',stocks[i])
+        print('ERR 404: STOCK NOT FOUND')
+        print('\n############################################################\n')
+        pass
+    else:
+        names.append(tables[0][1][2].replace(' ON','').replace(' PNA','').replace(' PNB','').replace(' PN','').replace(' N1','').replace(' UNT','').replace(' N2','').replace(' NM',''))
+        print('Empresa:',tables[0][1][2].replace(' ON','').replace(' PNA','').replace(' PNB','').replace(' PN','').replace(' N1','').replace(' UNT','').replace(' N2','').replace(' NM',''))
+        tickers.append(stocks[i])
+        print('Ativo:',stocks[i])
+        price.append(float(tables[0][3][0])/100)
+        print('Cotação --->',float(tables[0][3][0])/100)
+        try:
+            evebit.append(float(tables[2][3][10].replace('.',''))/100)
+            print('EV/EBIT  --->',float(tables[2][3][10].replace('.',''))/100)
+        except ValueError:
+            evebit.append(float('nan'))
+            print('EV/EBIT  --->',tables[2][3][10])
+        try:
+            m_ebit.append(float(tables[2][5][4].replace(',','.').replace('%','')))
+            print('M. EBIT  --->',tables[2][5][4].replace(',','.').replace('%',''))
+        except ValueError:
+            m_ebit.append(float('nan'))
+            print('M. EBIT  --->',tables[2][5][4].replace(',','.').replace('%',''))
+        volume.append(float(tables[0][3][4].replace('.','')))
+        print('Vol Méd  --->', float(tables[0][3][4].replace('.','')))
+        sit.append(situacao(stocks[i]))
+        print('\n############################################################\n')
+# build a dataframe with data from all stocks
+rows = {'Empresa':names, 'Ticker':tickers, 'Cotação':price, 'EV/EBIT':evebit, 'M.EBIT':m_ebit, 'Volume Médio':volume, 'Situação':sit}
+df = pd.DataFrame(rows)
+# Print first 50 data frame lines
+print(df.head(50))
+# Write the data to an output csv file
+df.to_csv("fundamentus.csv",index=False)
+shutil.move('fundamentus.csv', 'csv/fundamentus.csv')
+# Write a backup csv file with today date
+today = str(datetime.datetime.now().date())
+df.to_csv('fundamentus-' + today + ".csv",index=False)
+shutil.move('fundamentus-' + today + ".csv", 'csv/history/fundamentus-' + today + ".csv")
---
+++ ss_statusinvest.py
@@ -0,0 +1,76 @@
+from lxml import html
+import pandas as pd
+import requests
+import datetime
+import shutil
+
+# Read file with stocks tickers
+def tickers(InputFile):
+    infile = open(InputFile, "r")
+    tickers = [line.split() for line in infile]
+    ticker = []
+    for i in tickers:
+        ticker.append(i[0])
+    return ticker
+
+# Build the row with values for each stock
+def indicadores(ticker):
+    # Define url to read
+    url   = requests.get('https://statusinvest.com.br/acoes/'+ticker)
+    # Strips html content
+    tree   = html.fromstring(url.content)
+    # company name
+    name = tree.xpath('//small/text()')[0]
+    print('Empresa: ',name)
+    print('Ativo:',ticker)
+    # Stock price
+    price_values = tree.xpath('//strong[@class="value"]/text()')
+    # html line with values for selected stock
+    indicadores_financeiros = tree.xpath('//strong[@class="value d-block lh-4 fs-4 fw-700"]/text()')
+    # Financial situation
+    situacao = tree.xpath('//strong[@class="main-badge mt-1 fs-2 p-1 red accent-4"]/text()')
+    # Format prices to float
+    price = float(price_values[0].replace(',','.'))
+    print('Cotação  --->',price)
+    # Format EV/EBIT
+    evebit  = indicadores_financeiros[5].replace(',','.')
+    print('EV/EBIT  --->',evebit)
+    # Format EBIT Margin
+    m_ebit = indicadores_financeiros[22].replace('.','').replace(',','.').replace('%','')
+    print('M. EBIT  --->',m_ebit)
+    # Format daily volume
+    if ticker=='ALUP11' or ticker=='SAPR11' or ticker=='TAEE11':
+        volume = float(price_values[6].replace('.','').replace(',','.'))
+    else:
+        volume = float(price_values[7].replace('.','').replace(',','.'))
+    print('Vol Méd  --->', volume)
+    print('Situação --->',situacao)
+    print('\n############################################################\n')
+    return name, ticker, price, evebit, m_ebit, volume, situacao
+
+stocks = tickers('tickers.dat')
+j=0
+names,tickers,price,evebit,m_ebit,volume,sit=[],[],[],[],[],[],[]
+for i in stocks:
+    j+=1
+    print('Scraping', j, 'out of', len(stocks),'from: statusinvest.com.br')
+    row = indicadores(i)
+    names.append(row[0])
+    tickers.append(row[1])
+    price.append(row[2])
+    evebit.append(row[3])
+    m_ebit.append(row[4])
+    volume.append(row[5])
+    sit.append(row[6])
+# Build a Dataframe with all stocks data
+rows = {'Empresa':names, 'Ticker':tickers, 'Cotação':price, 'EV/EBIT':evebit, 'M.EBIT':m_ebit, 'Volume Médio':volume, 'Situação':sit}
+df=pd.DataFrame(rows)#, columns=header)
+# Print first 50 data frame lines
+print(df.head(50))
+# Write the data to an output csv file
+df.to_csv("statusinvest.csv",index=False)
+shutil.move('statusinvest.csv', 'csv/statusinvest.csv')
+# Write a backup csv file with today date
+today = str(datetime.datetime.now().date())
+df.to_csv('statusinvest-' + today + ".csv",index=False)
+shutil.move('statusinvest-' + today + ".csv", 'csv/history/statusinvest-' + today + ".csv")
