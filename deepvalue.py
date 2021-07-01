7338aa865718dc2552054a5836a178952a25ea42
cammneto <cammneto@gmail.com>
2021-07-01 14:28:43 -0300

    modified:   deepvalue.py 	modified:   sstools.py 	modified:   tickers.dat

--- deepvalue.py
+++ deepvalue.py
@@ -1,34 +1,37 @@
 import pandas as pd
+from tabulate import tabulate
 from sstools import *
 
 df1 = csv_to_df('csv/statusinvest.csv')
 df2 = csv_to_df('csv/investsite.csv')
 df3 = csv_to_df('csv/fundamentus.csv')
-#print(df1,df2,df3)
+#print(df1),print(df2),print(df3)
 
 df=pd.concat([df1,df2,df3],axis=1, ignore_index=True)
 df=rec_jud(df)
 
-df1,df2,df3=df.iloc[0:,:6],df.iloc[0:,6:12],df.iloc[0:,12:]
-df1,df2,df3=df1.rename(columns={0:'Empresa', 1:'Ticker', 2:'Cotação', 3:'EV/EBIT', 4:'M.EBIT', 5:'Vol.(Mi)'}),df2.rename(columns={7:'Empresa', 8:'Ticker', 9:'Cotação', 10:'EV/EBIT', 11:'M.EBIT', 12:'Vol.(Mi)'}),df3.rename(columns={14:'Empresa', 15:'Ticker', 16:'Cotação', 17:'EV/EBIT', 18:'M.EBIT', 19:'Vol.(Mi)'})
+df=low_vol(df)
+df=ebit_margin(df)
 
-df1,df2,df3=filter(df1,'Empresa'),filter(df2,'Empresa'),filter(df3,'Empresa')
-df_new=pd.DataFrame({'Empresa':df[0], 'Ticker':df[1], 'Cotação':round((df[2]+df[9]+df[16])/3,2), 'EV/EBIT':round((df[3]+df[10]+df[17])/3,2), 'M.EBIT':round((df[4]+df[11]+df[18])/3,2), 'Vol.(Mi)':round((df[5]+df[12]+df[19])/3,2)})
-df_new=filter(df_new,'Empresa')
+df1,df2,df3=df.iloc[0:,:6],df.iloc[0:,6:12],df.iloc[0:,12:]
+df1,df2,df3=df1.rename(columns={0:'Empresa', 1:'Ticker', 2:'Price', 3:'E.Y.', 4:'M.EBIT', 5:'Vol.(Mi)'}),df2.rename(columns={7:'Empresa', 8:'Ticker', 9:'Price', 10:'E.Y.', 11:'M.EBIT', 12:'Vol.(Mi)'}),df3.rename(columns={14:'Empresa', 15:'Ticker', 16:'Price', 17:'E.Y.', 18:'M.EBIT', 19:'Vol.(Mi)'})
+df1,df2,df3=higher_liq(df1),higher_liq(df2),higher_liq(df3)
+df_new=pd.DataFrame({'Empresa':df[0], 'Ticker':df[1], 'Price':round((df[2]+df[9]+df[16])/3,2), 'E.Y.':round((df[3]+df[10]+df[17])/3,2), 'M.EBIT':round((df[4]+df[11]+df[18])/3,2), 'Vol.(Mi)':round((df[5]+df[12]+df[19])/3,2)})
+df_new=higher_liq(df_new)
+
 ### OUTPUT Files ####
-csv_save(df1,'main_test')
-df1.to_csv("deepvalue_statusinv.csv",index=False)
-shutil.move('deepvalue_statusinv.csv', 'csv/dv_statusinv.csv')
-df2.to_csv("deepvalue_fundamentus.csv",index=False)
-shutil.move('deepvalue_fundamentus.csv', 'csv/dv_fundamentus.csv')
-df3.to_csv("deepvalue_investsite.csv",index=False)
-shutil.move('deepvalue_investsite.csv', 'csv/dv_investsite.csv')
-df_new.to_csv("deepvalue_filtered.csv",index=False)
-shutil.move('deepvalue_filtered.csv', 'csv/dv_filtered.csv')
+df1.to_csv("dv_statusinv.csv",index=False), shutil.move('dv_statusinv.csv', 'csv/dv_statusinv.csv')
+df2.to_csv("dv_investsite.csv",index=False), shutil.move('dv_investsite.csv', 'csv/dv_investsite.csv')
+df3.to_csv("dv_fundamentus.csv",index=False), shutil.move('dv_fundamentus.csv', 'csv/dv_fundamentus.csv')
+df_new.to_csv("dv_crossed.csv",index=False), shutil.move('dv_crossed.csv', 'csv/dv_crossed.csv')
 #### OUTPUT SCREEN ####
-bar = pd.DataFrame({'| | | ':40*['| | | ']})
-print('\n    statusinvest.com.br                          | | |   fundamentus.com.br                          | | |    investsite.com.br                          | | |   Média dos 3 sites')
-print('-------------------------------------------------| | |-----------------------------------------------| | |-----------------------------------------------| | |------------------------------------')
-print(pd.concat([df1, bar, df2, bar, df3, bar, df_new], axis=1).head(20))
-print('-------------------------------------------------| | |-----------------------------------------------| | |-----------------------------------------------| | |------------------------------------')
-print(pd.concat([df1, ' '+bar, df2, bar, df3, bar, df_new], axis=1)[20:40])
+rank = pd.concat([df1.iloc[0:,:4], df2.iloc[0:,:4], df3.iloc[0:,:4], df_new.iloc[0:,:4]], axis=1)
+rank.index = rank.index +1
+
+print('--  ---------- Status Invest ----------  ----------- Investsite ------------  ----------- Fundamentus -----------  ---------- 3 Sites Mean -----------')
+print(tabulate(rank.head(20), headers='keys', floatfmt=".2f"))#, tablefmt='plain'))
+print('--  -----------------------------------  -----------------------------------  -----------------------------------  -----------------------------------')
+print(tabulate(rank[20:30], headers='keys', floatfmt=".2f"))#, tablefmt='plain'))
+print('--  -----------------------------------  -----------------------------------  -----------------------------------  -----------------------------------')
+print(tabulate(rank[30:40], headers='keys', floatfmt=".2f"))#, tablefmt='plain'))
+
--- sstools.py
+++ sstools.py
@@ -18,11 +18,19 @@ def rec_jud(df):
     df=df.drop([6,13],axis=1)
     return df
 
-def filter(df,drop_col):
-    df=df.dropna().reset_index(drop=True)
-    df.drop(df[df['Vol.(Mi)'] < 0.2].index, inplace = True)
-    df.drop(df[df['EV/EBIT'] < 0].index, inplace = True)
-    df.drop(df[df['M.EBIT'] < 0].index, inplace = True)
+def low_vol(df):
+    df.drop(df[df[5] < 0.2].index, inplace = True)
+    df.drop(df[df[12] < 0.2].index, inplace = True)
+    df.drop(df[df[19] < 0.2].index, inplace = True)
+    return df
+
+def ebit_margin(df):
+    df.drop(df[df[4] < 0].index, inplace = True)
+    df.drop(df[df[11] < 0].index, inplace = True)
+    df.drop(df[df[18] < 0].index, inplace = True)
+    return df
+
+def higher_liq(df):
     df=df.sort_values(by=['Empresa','Vol.(Mi)']).reset_index(drop=True)
     for i in range(1,len(df['Empresa'])):
         if df['Empresa'][i-1]==df['Empresa'][i]:
@@ -30,10 +38,29 @@ def filter(df,drop_col):
             df=df[df['Ticker'].str.contains(remover)==False]
         else:
             pass
-    df=df.sort_values(by=['EV/EBIT'])
+    df=df.drop('Empresa',axis=1)
+    df=df.sort_values(by=['E.Y.'])
+    df['E.Y.']=100/df['E.Y.']
     df=df.reset_index(drop=True)
-    df=df.drop(drop_col,axis=1)
     return df
+
+
+#def filter(df,drop_col):
+#    df=df.dropna().reset_index(drop=True)
+#    df.drop(df[df['Vol.(Mi)'] < 0.2].index, inplace = True)
+#    df.drop(df[df['E.Y.'] < 0].index, inplace = True)
+#    df.drop(df[df['M.EBIT'] < 0].index, inplace = True)
+#    df=df.sort_values(by=['Empresa','Vol.(Mi)']).reset_index(drop=True)
+#    for i in range(1,len(df['Empresa'])):
+#        if df['Empresa'][i-1]==df['Empresa'][i]:
+#            remover=df['Ticker'][i-1]
+#            df=df[df['Ticker'].str.contains(remover)==False]
+#        else:
+#            pass
+#    df=df.sort_values(by=['E.Y.'])
+#    df=df.reset_index(drop=True)
+#    df=df.drop(drop_col,axis=1)
+#    return df
 
 def csv_save(df,file):
     df.to_csv("dv_"+file+".csv",index=False)
