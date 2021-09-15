49142737ef7d2da12f32694738de4b2c433e39ce
cammneto <cammneto@gmail.com>
2021-09-15 09:19:26 -0300

    modified:   stock_screener/ss_fundamentus.py

--- stock_screener.sh
+++ stock_screener.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-python3 stock_screener/ss_investsite.py
 python3 stock_screener/ss_fundamentus.py
+#python3 stock_screener/ss_investsite.py
 python3 stock_screener/ss_statusinvest.py
 
--- stock_screener/ss_fundamentus.py
+++ stock_screener/ss_fundamentus.py
@@ -30,9 +30,8 @@ miss, names, tickers, price, evebit, m_ebit, volume = ([] for i in range(7))
 for i in range(len(stocks)):
     tables = page_tables(stocks[i])
     #print(tables)
-    print('Scraping',i+1, 'out of', len(stocks),'from fundamentus.com.br', stocks[i])
+    print('Scraping', stocks[i],'from fundamentus.com.br', i+1, 'out of', len(stocks))
     if tables==None:
-        #print('Ativo:',stocks[i])
         print('ERR 404: STOCK NOT FOUND')
         print('\n############################################################\n')
         names.append('NOT FOUND')
@@ -43,40 +42,26 @@ for i in range(len(stocks)):
         volume.append(float('nan'))
         miss.append(stocks[i])
     else:
+        names.append(tables[0][1][2].replace(' ON','').replace(' PNA','').replace(' PNB','').replace(' PN','').replace(' N1','').replace(' UNT','').replace(' N2','').replace(' NM',''))
+        print('Empresa:',tables[0][1][2].replace(' ON','').replace(' PNA','').replace(' PNB','').replace(' PN','').replace(' N1','').replace(' UNT','').replace(' N2','').replace(' NM',''))
+        tickers.append(stocks[i])
+        price.append(float(tables[0][3][0])/100)
+        print('Cotação --->',float(tables[0][3][0])/100)
         try:
-            names.append(tables[0][1][2].replace(' ON','').replace(' PNA','').replace(' PNB','').replace(' PN','').replace(' N1','').replace(' UNT','').replace(' N2','').replace(' NM',''))
-            print('Empresa:',tables[0][1][2].replace(' ON','').replace(' PNA','').replace(' PNB','').replace(' PN','').replace(' N1','').replace(' UNT','').replace(' N2','').replace(' NM',''))
-            tickers.append(stocks[i])
-            #print('Ativo:',stocks[i])
-            price.append(float(tables[0][3][0])/100)
-            print('Cotação --->',float(tables[0][3][0])/100)
-            try:
-                evebit.append(float(tables[2][3][10].replace('.',''))/100)
-                print('EV/EBIT  --->',float(tables[2][3][10].replace('.',''))/100)
-            except ValueError:
-                evebit.append(float('nan'))
-                print('EV/EBIT  --->',tables[2][3][10])
-            try:
-                m_ebit.append(float(tables[2][5][4].replace(',','.').replace('%','')))
-                print('M. EBIT  --->',tables[2][5][4].replace(',','.').replace('%',''))
-            except ValueError:
-                m_ebit.append(float('nan'))
-                print('M. EBIT  --->',tables[2][5][4].replace(',','.').replace('%',''))
-                volume.append(float(tables[0][3][4].replace('.','')))
-                print('Vol Méd  --->', float(tables[0][3][4].replace('.','')))
-                print('\n############################################################\n')
-        except KeyError:
-            print('KeyError')
-            print('Ativo:',stocks[i])
-            print('ERR 404: STOCK NOT FOUND')
-            print('\n############################################################\n')
-            names.append('NOT FOUND')
-            tickers.append(stocks[i])
-            price.append(float('nan'))
+            evebit.append(float(tables[2][3][10].replace('.',''))/100)
+            print('EV/EBIT  --->',float(tables[2][3][10].replace('.',''))/100)
+        except ValueError:
             evebit.append(float('nan'))
+            print('EV/EBIT  --->',tables[2][3][10])
+        try:
+            m_ebit.append(float(tables[2][5][4].replace(',','.').replace('%','')))
+            print('M. EBIT  --->',tables[2][5][4].replace(',','.').replace('%',''))
+        except ValueError:
             m_ebit.append(float('nan'))
-            volume.append(float('nan'))
-            miss.append(stocks[i])
+        print('M. EBIT  --->',tables[2][5][4].replace(',','.').replace('%',''))
+        volume.append(float(tables[0][3][4].replace('.','')))
+        print('Vol Méd  --->', float(tables[0][3][4].replace('.','')))
+        print('\n############################################################\n')
 # build a dataframe with data from all stocks
 print("ERROR: page not found",miss)
 rows = {'Empresa':names, 'Ticker':tickers, 'Cotação':price, 'EV/EBIT':evebit, 'M.EBIT':m_ebit, 'Volume Médio':volume}
